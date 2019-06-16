7fa42b9c366ceb30710d8374075f288df2abfc6a
Tom Lianza <tlianza@cloudflare.com>
2019-06-15 20:45:51 -0700

    Updates

---
+++ precache-manifest.9dcf934687cf1e717295dd430f91970f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ba54a931aabe7243623f0b9c2909e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "454e8cd4d6f71b2aa04e",
+    "url": "/static/css/2.d444ac8c.chunk.css"
+  },
+  {
+    "revision": "b8173ade7f4cf2527451",
+    "url": "/static/css/main.31f741ee.chunk.css"
+  },
+  {
+    "revision": "454e8cd4d6f71b2aa04e",
+    "url": "/static/js/2.47d37350.chunk.js"
+  },
+  {
+    "revision": "b8173ade7f4cf2527451",
+    "url": "/static/js/main.a12c3139.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2ec8b5247a27284077c8e3dc42c1cef.js"
+  "/precache-manifest.9dcf934687cf1e717295dd430f91970f.js"
 );
 
 self.addEventListener('message', (event) => {
