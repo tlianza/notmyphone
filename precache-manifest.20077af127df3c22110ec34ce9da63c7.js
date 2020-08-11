9ab0ada1f196f691ba59a2174ec11d793fe7dcef
Tom Lianza <tlianza@cloudflare.com>
2020-08-10 21:22:22 -0700

    Updates

---
+++ precache-manifest.20077af127df3c22110ec34ce9da63c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e5d2eba0b0a160bc3ac21ea69ef2ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0870173ca6f49fefdc",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3668224976e6c7049201",
+    "url": "/static/css/main.795cbe6c.chunk.css"
+  },
+  {
+    "revision": "5f0870173ca6f49fefdc",
+    "url": "/static/js/2.994c031b.chunk.js"
+  },
+  {
+    "revision": "3668224976e6c7049201",
+    "url": "/static/js/main.d163ca70.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1932b75d1492a09b34479a96bcd74dfe.js"
+  "/precache-manifest.20077af127df3c22110ec34ce9da63c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
